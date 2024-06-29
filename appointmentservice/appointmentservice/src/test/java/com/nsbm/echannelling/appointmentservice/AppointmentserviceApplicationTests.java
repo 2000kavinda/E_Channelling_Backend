b811605a4b04c39efd1361f9df8217cbd630c0b2
2000kavinda <98000159+2000kavinda@users.noreply.github.com>
2024-06-29 09:46:48 +0530

    change project structure to micro service

---
+++ appointmentservice/appointmentservice/src/test/java/com/nsbm/echannelling/appointmentservice/AppointmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.echannelling.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ labservice/labservice/src/test/java/com/nsbm/echannelling/labservice/LabserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.echannelling.labservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
