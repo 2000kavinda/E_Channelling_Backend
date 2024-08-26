fb12478e4951bab058ba2f91ed265fbbf96aabcf
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2024-08-26 21:22:09 +0530

    Create cross origin

--- doctorservice/doctorservice/src/main/java/com/nsbm/echannelling/doctorservice/controller/BookingController.java
+++ doctorservice/doctorservice/src/main/java/com/nsbm/echannelling/doctorservice/controller/BookingController.java
@@ -4,14 +4,12 @@ import com.nsbm.echannelling.doctorservice.model.Booking;
 import com.nsbm.echannelling.doctorservice.service.BookingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/v1/bookings")
 public class BookingController {
