354bd7576f8b4bd6b72f8ccd342475acfd091a16
2000kavinda <98000159+2000kavinda@users.noreply.github.com>
2024-09-03 02:29:23 +0530

    cross origin added

--- authenticationservice/authenticationservice/src/main/java/com/nsbm/echannelling/authenticationservice/controller/AuthController.java
+++ authenticationservice/authenticationservice/src/main/java/com/nsbm/echannelling/authenticationservice/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("api/v1/auth")
 public class AuthController {
 
